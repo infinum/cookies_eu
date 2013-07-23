d5a5f1ec74a68593ca6bb7f2f339a3460dd5e67e
Stjepan Hadjic <d4be4st@gmail.com>
2013-07-23 14:05:02 +0200

    fixed typo

--- app/assets/javascripts/cookies_eu.js
+++ app/assets/javascripts/cookies_eu.js
@@ -1,7 +1,6 @@
 //= require jquery
 //= require jquery.cookie
 //= require jquery.mobile.custom
-//= require jquery.mobile.custom.structure
 
 $(document).ready( function(){
   $('.cookies-eu-ok').click(function(e){
