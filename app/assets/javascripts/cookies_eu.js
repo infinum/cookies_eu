91ac7241ba3d85772886c883416333cec6d7b91c
Stjepan Hadjic <d4be4st@gmail.com>
2013-07-23 16:47:36 +0200

    cookies now have expiration of one year

--- app/assets/javascripts/cookies_eu.js
+++ app/assets/javascripts/cookies_eu.js
@@ -4,7 +4,7 @@
 $(document).ready( function(){
   $('.cookies-eu-ok').click(function(e){
     e.preventDefault();
-    $.cookie('cookie_eu_consented', 'true', { path: '/'});
+    $.cookie('cookie_eu_consented', 'true', { path: '/', expires: 365 });
     $('.cookies-eu').remove();
   });
 });
