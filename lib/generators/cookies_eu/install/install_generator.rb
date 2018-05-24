46ad3ea7062f11ff06c6d969d7538a1971789832
Gabrijel Škoro <gabrijel.skoro@gmail.com>
2018-05-24 23:17:31 +0200

    installing cookie with generator would fail if we had a .scss file

--- lib/generators/cookies_eu/install/install_generator.rb
+++ lib/generators/cookies_eu/install/install_generator.rb
@@ -6,7 +6,18 @@ module CookiesEu
       end
 
       def add_stylesheets
-        inject_into_file 'app/assets/stylesheets/application.css', "*= require cookies_eu\n", before: /\*\//, verbose: true
+        file = 'app/assets/stylesheets/application'
+        css_file = "#{file}.css"
+        scss_file = "#{file}.scss"
+        css_scss_file = "#{file}.css.scss"
+
+        if File.exists?(css_file)
+          inject_into_file css_file, "*= require cookies_eu\n", before: /\*\//, verbose: true
+        elsif File.exists?(scss_file)
+          append_file scss_file, "@import 'cookies_eu';\n"
+        else
+          append_file css_scss_file, "@import 'cookies_eu';\n"
+        end
       end
     end
   end
